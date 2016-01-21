5d2c36ee3967e838c3d61f686f84b672dfd5d22d
Daniel Bartholomae <daniel@bartholomae.name>
2016-01-21 13:43:06 +0100

    fix: add debug info

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 var path = require('path');
+var debug = require('debug')('run-mod-script');
 
 module.exports = function(scriptRunner, dir, options){
+  debug("Starting run-mod-script");
   var cwd = process.cwd();
+  debug(" cwd: " + cwd);
+  debug(" dir: " + dir);
 
   var argv = require('minimist')(process.argv.slice(2));
   var files = argv._.map(function(file){
     return(path.relative(dir, path.join(cwd, file)));
   });
 
+  debug(" files: " + files);
+
   delete argv._;
 
   for (key in options) {
     argv[key] = options[key];
   }
+
+  debug(" options: " + JSON.stringify(options, null, 2));
 
   process.argv = process.argv.slice(0, 2);
   for (var key in argv) {
@@ -21,7 +29,14 @@ module.exports = function(scriptRunner, dir, options){
   }
   process.argv = process.argv.concat(files);
 
-  process.chdir(path.relative(cwd, dir));
+  var relToDir = path.relative(cwd, dir);
+  var relFromDir = path.relative(dir, cwd);
+  debug(" changing path to dir: " + relToDir);
+  if(relToDir !== "")
+    process.chdir(relToDir);
+  debug(" running script");
   scriptRunner();
-  process.chdir(path.relative(dir, cwd));
+  debug(" changing path from dir: " + relToDir);
+  if(relFromDir !== "")
+    process.chdir(relFromDir);
 }
